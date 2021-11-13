5281d5a3de392d3cef445ab4002d7fcecf7df740
Anubis <anubisawooo@163.com>
2021-11-13 23:45:12 +0800

    vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  root: './src/icp_web_assets/src', // vite 执行的根目录
+  publicDir: '../assets',
+  build: {
+    outDir: '../../../dist/icp_web_assets', // 构建输出目录
+  }
 })
