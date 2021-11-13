2e674b4ff8c2a9eac9db22fef784a843f75f55d3
Anubis <anubisawooo@163.com>
2021-11-13 22:46:50 +0800

    vite

--- src/icp_web_assets/src/index.js
+++
@@ -1,9 +0,0 @@
-import { icp_web } from "../../declarations/icp_web";
-
-document.getElementById("clickMeBtn").addEventListener("click", async () => {
-  const name = document.getElementById("name").value.toString();
-  // Interact with icp_web actor, calling the greet method
-  const greeting = await icp_web.greet(name);
-
-  document.getElementById("greeting").innerText = greeting;
-});
