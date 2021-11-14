8dd5e63f5cbb640c5f2f6ebd469fb7eb8a165190
Anubis <anubisawooo@163.com>
2021-11-14 23:07:10 +0800

    config

--- vite.config.ts
+++ vite.config.ts
@@ -2,38 +2,53 @@ import { defineConfig, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from "path";
 
+// 区分环境配置，关键点在于开发环境需要哪些东西，这些东西怎么来？
+// 主要获取的信息有 需要用到的一些 canister id 和 这些 id 对应的接口，把这类文件找对
+// 对于 id 的读取，我希望配置到一个确定的路径数组中，全部加入
+// 对于 接口 的读取，同样是路径数组，全部加入
+
+
+
+function initCanisterIdsAndAlias(isDevelopment: boolean) {
+  let localCanisters: any, prodCanisters: any, canisters: any;
+
+  try {
+    // 读取本地开发环境的 canister id 路径是 ./.dfx/local/canister_ids.json
+    localCanisters = require(path.resolve(".dfx", "local", "canister_ids.json"));
+  } catch (error) {
+    console.log("No local canister_ids.json found. Continuing production");
+  }
+  try {
+    // 读取生产环境的 canister id 路径是 ./canister_ids.json
+    prodCanisters = require(path.resolve("canister_ids.json"));
+  } catch (error) {
+    console.log("No production canister_ids.json found. Continuing with local");
+  }
+
+  const network = process.env.DFX_NETWORK || (isDevelopment ? "local" : "ic");
+
+  canisters = network === "local" ? localCanisters : prodCanisters;
+
+  for (const canister in canisters) {
+    process.env[canister.toUpperCase() + "_CANISTER_ID"] =
+      canisters[canister][network];
+  }
+  let canistersAlias = {};
+  for (const canister in canisters) {
+    canistersAlias['dfx-generated/' + canister] = path.join(__dirname, ".dfx", network, "canisters", canister);
+  }
+
+  return {
+    network,
+    canistersAlias,
+  }
+}
+
 export default defineConfig(({ command, mode }) => {
   let isDevelopment = mode !== 'production';
-  let network = 'local';
-  let canistersAlias = {};
+
   // 初始化 canisterId 进入环境
-  function initCanisterIds() {
-    let localCanisters: any, prodCanisters: any, canisters: any;
-
-    try {
-      localCanisters = require(path.resolve(".dfx", "local", "canister_ids.json"));
-    } catch (error) {
-      console.log("No local canister_ids.json found. Continuing production");
-    }
-    try {
-      prodCanisters = require(path.resolve("canister_ids.json"));
-    } catch (error) {
-      console.log("No production canister_ids.json found. Continuing with local");
-    }
-
-    network = process.env.DFX_NETWORK || (isDevelopment ? "local" : "ic");
-
-    canisters = network === "local" ? localCanisters : prodCanisters;
-
-    for (const canister in canisters) {
-      process.env[canister.toUpperCase() + "_CANISTER_ID"] =
-        canisters[canister][network];
-    }
-    for (const canister in canisters) {
-      canistersAlias['dfx-generated/' + canister] = path.join(__dirname, ".dfx", network, "canisters", canister);
-    }
-  }
-  initCanisterIds();
+  let {network, canistersAlias} = initCanisterIdsAndAlias(isDevelopment);
 
   let common: UserConfig = {
     root: './src/icp_web_assets/src', // vite 执行的根目录
@@ -72,6 +87,7 @@ export default defineConfig(({ command, mode }) => {
       outDir: '../../../dist/icp_web_assets', // 构建输出目录
       minify: mode !== 'production' ? false : 'terser',
     },
+    envDir: './.env' // 加载环境变量
   };
   if (command === 'serve') {
     return {
